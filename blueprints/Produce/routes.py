eea2a77e0897217bba2df886a91fc9f12d535d1e
Himanshu Sharma <tsk213@alumni.ku.dk>
2024-06-07 19:29:27 +0200

    Added search

--- blueprints/Produce/routes.py
+++ blueprints/Produce/routes.py
@@ -24,6 +24,19 @@ def produce():
         title = f'Our {request.form.get("category")}!'
     return render_template('pages/properties.html', produce=produce, form=form, title=title)
 
+@Produce.route("/Search Properties", methods=['GET', 'POST'])
+def property():
+    form = FilterPropertyForm()
+    title = 'Our properties!'
+    produce = []
+    if request.method == 'POST':
+        produce = get_produce_by_filters(category=request.form.get('category'),
+                                         item=request.form.get('item'),
+                                         variety=request.form.get('variety'),
+                                         farmer_name=request.form.get('sold_by'),
+                                         price=request.form.get('price'))
+        title = f'Our {request.form.get("category")}!'
+    return render_template('pages/search.html', produce=produce, form=form, title=title)
 
 @Produce.route("/add-produce", methods=['GET', 'POST'])
 @login_required
